4358b2199d9b958711783a807e57dda3b809fb83
Gurkan Tongel <gurkantngl@gmail.com>
2024-04-22 14:06:34 +0300

    6 kelimeli oyun ekranı eklendi

--- app/src/main/java/com/gurkantngl/wordgame/ui/FiveGameActivity.kt
+++ app/src/main/java/com/gurkantngl/wordgame/ui/FiveGameActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gurkantngl.wordgame.R
 
-class ActivityFiveGame : AppCompatActivity() {
+class FiveGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
