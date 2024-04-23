10dcc353c99514c1382521354ce6996ebf38e4c7
Gurkan Tongel <gurkantngl@gmail.com>
2024-04-23 19:12:03 +0300

    UI Update

---
+++ app/src/main/java/com/gurkantngl/wordgame/ui/SevenGameActivity.kt
@@ -0,0 +1,21 @@
+package com.gurkantngl.wordgame.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.gurkantngl.wordgame.R
+
+class SevenGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seven_game)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
